a46bd634a83b70cbd1ba126f3bde8f6d6f6b311f
murray501 <m68286832@gmail.com>
2021-12-21 12:01:10 +0900

    add message list

--- client/src/home.js
+++ client/src/home.js
@@ -1,10 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import { io } from "socket.io-client";
 import { useInput } from "./hooks";
 
 export default function Home() {
     const [messageProps, resetMessage] = useInput("");
     const [socket, setSocket] = useState();
+    const [messages, setMessage] = useReducer(
+        (messages, newMessage) => [...messages, newMessage],
+        []
+    );
 
     const submit = e => {
         e.preventDefault();
@@ -15,15 +19,17 @@ export default function Home() {
     useEffect(() => {
         const socket = io("http://localhost:5000");
         socket.on("connect", () => {
-            console.log(socket.id)
+            console.log(socket.id);
         })
         socket.on('chat message', msg => {
             console.log('message: ' + msg);
+            setMessage(msg);
         })
         setSocket(socket)
     }, [])
 
     return (
+        <>
         <form onSubmit={submit}>
             <input
                 {...messageProps}
@@ -33,5 +39,13 @@ export default function Home() {
             />
             <button>SEND</button>
         </form>
+        <ul>
+            {messages.map((message, i) => (
+                <li>
+                    {message}
+                </li>
+            ))}
+        </ul>
+        </>
     )
 }
